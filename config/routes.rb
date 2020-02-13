fa2b51c7195f40b046867555146505532696067f
Dobashi, Hiroki <hiroki.dobashi@gmail.com>
2020-02-13 13:33:32 +0900

    sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/logged_in", to: "sessions#is_logged_in?"
   resources :users, only: [:create, :show, :index]
 end
